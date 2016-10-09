a046559b2534ef890b1499e64c066a62729ccaaa
Arjit Jaiswal <arjitj2@gmail.com>
2016-10-08 23:27:07 -0400

    finished translation

--- Java_Test_Files/src/Computer_Vision/helper.java
+++ Java_Test_Files/src/Computer_Vision/helper.java
@@ -1,7 +1,11 @@
 package Computer_Vision;
 
+import java.util.ArrayList;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import com.memetix.mst.language.Language;
+import com.memetix.mst.translate.Translate;
 
 public class helper {
 	public static boolean has_text(JSONArray categories) {
@@ -18,5 +22,31 @@ public class helper {
 			}
 		}
 		return false;
+	}
+	
+	public static JSONArray add_translations(JSONObject text_dict) throws Exception {
+		JSONArray translated_dict = new JSONArray();
+        JSONArray regions = (JSONArray) text_dict.get("regions");
+        for(int i = 0; i < regions.size(); i++) {
+        	String original = "";
+        	JSONObject region = (JSONObject) regions.get(i);
+        	JSONArray lines = (JSONArray) region.get("lines");
+        	for(int j=0; j< lines.size(); j++) {
+        		JSONObject line = (JSONObject) lines.get(j);
+            	JSONArray words = (JSONArray) line.get("words");
+            	for(int k = 0; k < words.size(); k++) {
+            		JSONObject word = (JSONObject) words.get(k);
+            		original += (word.get("text") + " ");
+            	}
+        	}
+        	
+        	Translate.setClientId("mhacks2016couchsquad");
+        	Translate.setClientSecret("ZDlol0NO05RJeClPXuam5YY9JRRi6bI3PcXyjF/kpWk=");
+        	String translated = Translate.execute(original, Language.AUTO_DETECT, Language.ENGLISH);
+        	region.put("translation", translated);
+
+        	translated_dict.add(i, region);
+        }
+		return translated_dict;
 	}
 }
--- Java_Test_Files/src/Computer_Vision/home.java
+++ Java_Test_Files/src/Computer_Vision/home.java
@@ -9,7 +9,7 @@ import org.json.simple.JSONObject;
 
 public class home {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		// TODO: Retrieve images from Glass
 		File fi = new File("/Users/arjit/Downloads/4signs.jpg");
 		byte[] fileContent = Files.readAllBytes(fi.toPath());
@@ -19,10 +19,13 @@ public class home {
         
         boolean has_text = helper.has_text(categories);
         
+        JSONArray translated_dict = new JSONArray();
         if(has_text) {
         	JSONObject text_dict = ocr.get_text(fileContent);
-        	System.out.println(text_dict);
+        	translated_dict = helper.add_translations(text_dict);
         }
+        
+        System.out.println(translated_dict);
 	}
 
 }
